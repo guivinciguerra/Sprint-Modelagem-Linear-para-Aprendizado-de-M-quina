574a6e1f5bf3de44a64e6580b288dd51aca1adb6
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-05-18 20:38:25 -0300

    fix: melhorando clareza dos insights

--- a.py
+++ a.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("detailed_ev_charging_stations.csv")
 
+# VARIÁVEL QUANTITATIVA CONTÍNUA
+
 coluna = "Cost (USD/kWh)"
 
-k = 6
+n = len(df[coluna].dropna())
+k = int(1 + 3.3 * np.log10(n))
 
 classes = pd.cut(df[coluna], bins=k)
 
@@ -14,22 +18,22 @@ freq_acum = freq_abs.cumsum()
 freq_rel_acum = freq_rel.cumsum()
 
 tabela_freq = pd.DataFrame({
-    "Custo em USD/kwh": freq_abs.index.astype(str),     
-    "Frequência Absoluta": freq_abs.values,
-    "Frequência Relativa (%)": freq_rel.round(2).values,
-    "Frequência Acumulada": freq_acum.values,
-    "Frequência Relativa Acumulada (%)": freq_rel_acum.round(2).values
+    "Custo em USD/kwh": freq_abs.index.astype(str),
+    "Freq. Absoluta": freq_abs.values,
+    "Freq. Relativa (%)": freq_rel.round(2).values,
+    "Freq. Acumulada": freq_acum.values,
+    "Freq. Relativa Acumulada (%)": freq_rel_acum.round(2).values
 })
 
+print("\nTABELA - VARIÁVEL CONTÍNUA")
 print(tabela_freq.to_string(index=False))
 
-# Nota-se grande variação de preços nas recargas, com números de estações extremamente próximos entre as faixas
-# Isto sugere que é possível realizar uma diminuição significativa nos preços de diversas estações de recarga, caso haja uma padronização.
+print("\n# Insight 1: Há distribuição relativamente equilibrada entre as faixas de preço.")
+print("# Insight 2: Existe potencial para padronização dos preços das recargas.")
 
+# VARIÁVEL QUANTITATIVA DISCRETA
 
 coluna2 = "Installation Year"
-
-k = 3
 
 freq_abs = df[coluna2].value_counts().sort_index()
 freq_rel = (freq_abs / freq_abs.sum()) * 100
@@ -37,14 +41,15 @@ freq_acum = freq_abs.cumsum()
 freq_rel_acum = freq_rel.cumsum()
 
 tabela_freq2 = pd.DataFrame({
-    "Valor": freq_abs.index,
-    "Frequência Absoluta": freq_abs.values,
-    "Frequência Relativa (%)": freq_rel.round(2).values,
-    "Frequência Acumulada": freq_acum.values,
-    "Frequência Relativa Acumulada (%)": freq_rel_acum.round(2).values
+    "Ano": freq_abs.index,
+    "Freq. Absoluta": freq_abs.values,
+    "Freq. Relativa (%)": freq_rel.round(2).values,
+    "Freq. Acumulada": freq_acum.values,
+    "Freq. Relativa Acumulada (%)": freq_rel_acum.round(2).values
 })
 
+print("\nTABELA - VARIÁVEL DISCRETA")
 print(tabela_freq2.to_string(index=False))
 
-# Percebe-se um aumento no número de estações de recarga criadas ao longo dos anos
-# Demonstrando a tendencia de aumento no mercado de carros eletricos
+print("\n# Insight 1: O número de estações cresce ao longo dos anos.")
+print("# Insight 2: O mercado de veículos elétricos demonstra forte expansão.")
